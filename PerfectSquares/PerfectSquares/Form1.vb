6ce67701a7b2101cbe4367f997348ca6fe6c4a0a
Jonathan Bowles <bowl0071@umn.edu>
2013-10-09 15:18:35 -0500

    Updated workday 6

--- PerfectSquares/PerfectSquares/Form1.vb
+++ PerfectSquares/PerfectSquares/Form1.vb
@@ -3,9 +3,9 @@
     Private Sub btnDisplay_Click(sender As System.Object, e As System.EventArgs) Handles btnDisplay.Click
         lstResult.Items.Clear()
         Dim persq As Integer = 1
-        Do While persq < 100
-            lstResult.Items.Add("Number is " & persq)
-            persq = (persq + 1) ^ 2
+        Do While persq < 11
+            lstResult.Items.Add("Number is " & persq ^ 2)
+            persq = (persq + 1)
         Loop
 
     End Sub

62baf46dc71dd52bf978d09d9b453414aa62dacc
Jonathan Bowles <bowl0071@umn.edu>
2013-10-09 15:17:16 -0500

    Jonathan Bowles In class exercise 6

--- PerfectSquares/PerfectSquares/Form1.vb
+++ PerfectSquares/PerfectSquares/Form1.vb
@@ -1,9 +1,12 @@
 ﻿Public Class Form1
 
     Private Sub btnDisplay_Click(sender As System.Object, e As System.EventArgs) Handles btnDisplay.Click
-
         lstResult.Items.Clear()
-        lstResult.Items.Add("Not implemented yet")
+        Dim persq As Integer = 1
+        Do While persq < 100
+            lstResult.Items.Add("Number is " & persq)
+            persq = (persq + 1) ^ 2
+        Loop
 
     End Sub
 End Class
--- PopulationSim/PopulationSim/Form1.vb
+++ PopulationSim/PopulationSim/Form1.vb
@@ -3,8 +3,16 @@
 Public Class Form1
 
     Private Sub btnPopulation_Click(sender As System.Object, e As System.EventArgs) Handles btnPopulation.Click
+        Dim pop As Double = 7000000000
+        Dim curyear As Integer = 2012
 
-        MessageBox.Show("Not implemented yet", "Error")
+        Do While (pop > 6000000)
+            pop = pop / 2
+            curyear = curyear - 50
+        Loop
+
+
+        MessageBox.Show("The year is " & curyear, "Year")
 
     End Sub
 End Class
